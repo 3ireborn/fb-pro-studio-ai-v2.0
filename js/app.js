bfb0deff1c005dc6fd32cfe7a08f0473b0bf47bd
3ireborn <pakdsugie3i@gmail.com>
2026-07-17 09:10:22 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,16 +1,24 @@
 /*
 =========================================
 FB PRO AI STUDIO V2.0
-
-Powered by
-NPA Smart System VVIP
-
-app.js
+Powered by NPA Smart System VVIP
 =========================================
 */
 
 document.addEventListener("DOMContentLoaded", function () {
 
     console.log("FB PRO AI STUDIO V2.0 Loaded");
+
+    const loadingScreen = document.getElementById("loadingScreen");
+
+    if (loadingScreen) {
+
+        setTimeout(function () {
+
+            loadingScreen.style.display = "none";
+
+        }, 1800);
+
+    }
 
 });
